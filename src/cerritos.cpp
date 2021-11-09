f308b3a4ec723f58149d5a825903b9cf83336866
Dave Fancella <davefancella@gmail.com>
2021-11-08 21:02:26 -0600

    First commit

---
+++ src/cerritos.cpp
@@ -0,0 +1,46 @@
+/*
+ * Cerritos
+ * Copyright 2021 by Dave Fancella, Anthony Fancella
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a 
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included 
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ * 
+ */
+
+#include "SDL.h"
+
+#include "cerritos.h"
+
+/** Initialize cerritos
+ */
+int cInit() {
+    if(SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO)==-1) {
+        printf("SDL_Init: %s\n", SDL_GetError());
+        return -1;
+    }
+    
+    return 1;
+};
+
+/** Close cerritos
+ */
+void cClose() {
+    SDL_Quit();
+};
+
+
